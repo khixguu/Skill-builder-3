aaa19ff7a0efcd1e1a545ae98c1c44774d49acd6
Kihangu Waweru <kw817373@gmail.com>
2024-02-20 15:49:07 -0500

    Skill bulider 3

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -9,5 +9,36 @@
  */
 public class Grok
 {
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+    /**
+     \* Initializes a Grok object to the default power level of 50.
+     */
+
+    public Grok(){
+        powerLevel = DEFAULT_POWER_LEVEL;
+    }
+
+    public Grok(int powerLevel){
+        setPowerLevel(powerLevel);
+    }
+
+    public int getPowerLevel(){
+        return powerLevel;
+    }
+
+    public void setPowerLevel(int powerLevel){
+        this.powerLevel = powerLevel;
+    }
+
+    public void takePowerPill(PowerPill pill){
+        this.powerLevel += pill.getPower();
+    }
+
+    public void tookHit(){
+        this.powerLevel -= 5;
+    }
+
+
     // implement this class per instructions in Required Activities.
 }
